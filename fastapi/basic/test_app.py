8582eefe52d6c7d53c454ea70336f3b008c5a686
jongwoo Yoo <dbwhddn10@gmail.com>
2022-02-01 03:50:48 +0900

    feat(fastapi): add basic example

---
+++ fastapi/basic/test_app.py
@@ -0,0 +1,15 @@
+from fastapi import Depends, FastAPI
+from fastapi.testclient import TestClient
+
+app = FastAPI()
+client = TestClient(app)
+
+
+@app.get("/")
+async def hello_world():
+    return "hello world"
+
+
+def test_case():
+    response = client.get("/")
+    assert response.json() == "hello world"
