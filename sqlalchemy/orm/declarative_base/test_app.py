e72e373c820c6e9db8f4824dbcd4d2929cef2f20
jongwoo Yoo <dbwhddn10@gmail.com>
2022-02-01 22:35:53 +0900

    feat(sqlalchemy): add orm/declarative_base example

---
+++ sqlalchemy/orm/declarative_base/test_app.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, inspect
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+def test_case():
+    assert User.__table__ == inspect(User).local_table
