b877cf2a1ac8bfae1fe2fc63529d596e04d3dbdf
jongwoo Yoo <dbwhddn10@gmail.com>
2022-02-03 00:46:46 +0900

    feat(sqlalchemy): add orm/selectin_polymorphic example

---
+++ sqlalchemy/orm/selectin_polymorphic/test_app.py
@@ -0,0 +1,68 @@
+import logging
+import re
+
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy.orm import Query, Session, declarative_base, selectin_polymorphic
+
+Base = declarative_base()
+
+
+class Keyword(Base):
+    __tablename__ = "keywords"
+    id = Column(Integer, primary_key=True)
+    type_of = Column(String(50))
+
+    __mapper_args__ = {"polymorphic_identity": "keyword", "polymorphic_on": type_of}
+
+
+class Nationality(Keyword):
+    __tablename__ = "nationalities"
+    id = Column(Integer, ForeignKey("keywords.id"), primary_key=True)
+    code = Column(String)
+    __mapper_args__ = {
+        "polymorphic_identity": "nationality",
+    }
+
+
+class Religion(Keyword):
+    __tablename__ = "religions"
+    id = Column(Integer, ForeignKey("keywords.id"), primary_key=True)
+    type = Column(String)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "religion",
+    }
+
+
+def test_case():
+
+    engine = create_engine("sqlite://")
+    connect = engine.connect()
+    session = Session(engine)
+    religion = Religion(id=1, type="christian")
+    nationality1 = Nationality(id=2, code="US")
+    nationality2 = Nationality(id=3, code="KR")
+    Base.metadata.create_all(engine)
+    session.add(religion)
+    session.add(nationality1)
+    session.add(nationality2)
+    session.commit()
+    messages = []
+
+    class StreamHandler(logging.StreamHandler):
+        def emit(self, record):
+            if re.match("^SELECT", record.msg):
+                messages.append(record.msg)
+
+    logging.basicConfig()
+    logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+    logging.getLogger("sqlalchemy.engine").addHandler(StreamHandler())
+
+    (
+        Query(Keyword)
+        .with_session(session)
+        .options(selectin_polymorphic(Keyword, [Nationality, Religion]))
+        .all()
+    )
+
+    assert len(messages) == 3
