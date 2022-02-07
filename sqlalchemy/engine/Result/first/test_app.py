d6e92053cca336cf52ead51e531faa1600eb10df
jongwoo Yoo <dbwhddn10@gmail.com>
2022-02-07 21:25:50 +0900

    feat(sqlalchemy): add engine/Result/first example

---
+++ sqlalchemy/engine/Result/first/test_app.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, create_engine, select
+from sqlalchemy.engine import Row
+from sqlalchemy.orm import Session, registry
+
+Base = registry().generate_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+def test_case():
+    engine = create_engine("sqlite://")
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    session.add(User(id=1, name="yoo"))
+    session.add(User(id=2, name="lee"))
+    session.add(User(id=3, name="park"))
+    session.commit()
+    query = select(User)
+    result = session.execute(query)
+    row = result.first()
+
+    assert isinstance(row, Row)
