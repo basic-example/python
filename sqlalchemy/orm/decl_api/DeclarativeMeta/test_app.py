e6ef10a7136b90b97ad1bddaf4e530c5b439aef4
jongwoo Yoo <dbwhddn10@gmail.com>
2022-02-26 13:06:07 +0900

    chore(sqlalchemy): remove decl_api/DeclarativeMeta example

--- sqlalchemy/orm/decl_api/DeclarativeMeta/test_app.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm.decl_api import DeclarativeMeta
-
-Base = declarative_base()
-
-
-def test_case():
-    assert isinstance(Base, DeclarativeMeta)
