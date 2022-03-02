8c6fd1400f747829d2ae4fe77b5f12cb512d3ed4
jongwoo Yoo <dbwhddn10@gmail.com>
2022-03-02 10:28:49 +0900

    feat(fastapi): update Depends example for nested case

--- fastapi/Depends/test_app.py
+++ fastapi/Depends/test_app.py
@@ -3,11 +3,23 @@ from fastapi.testclient import TestClient
 
 app = FastAPI()
 client = TestClient(app)
+customDict = {"x": 10, "y": 20}
+
+
+class CustomClass:
+    def __init__(self, data=Depends(lambda: customDict)):
+        self.prop = "something"
+        self.data = data
 
 
 class QueryParams:
-    def __init__(self, ids: list[int] = Query([])):
+    def __init__(
+        self,
+        ids: list[int] = Query([]),
+        obj=Depends(CustomClass),
+    ):
         self.ids = ids
+        self.obj = obj
 
 
 class ListParams:
@@ -29,7 +41,21 @@ async def hello_world(params: ListParams = Depends()):
 
 def test_case():
     response = client.get("/")
-    assert response.json() == {"limit": 10, "skip": 0, "query": {"ids": []}}
+    assert response.json() == {
+        "limit": 10,
+        "skip": 0,
+        "query": {
+            "obj": {"prop": "something", "data": {"x": 10, "y": 20}},
+            "ids": [],
+        },
+    }
 
     response = client.get("/", params={"ids": [1, 2]})
-    assert response.json() == {"limit": 10, "skip": 0, "query": {"ids": [1, 2]}}
+    assert response.json() == {
+        "limit": 10,
+        "skip": 0,
+        "query": {
+            "obj": {"prop": "something", "data": {"x": 10, "y": 20}},
+            "ids": [1, 2],
+        },
+    }
