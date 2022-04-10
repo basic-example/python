55b8ad7a55a749bf8299ab44456847869fdc37e0
jongwoo Yoo <dbwhddn10@gmail.com>
2022-04-10 14:30:43 +0900

    feat(pillow): add Image/Image/mode example

---
+++ pillow/Image/Image/mode/test_app.py
@@ -0,0 +1,11 @@
+import base64
+from io import BytesIO
+
+from PIL import Image
+
+
+def test_case():
+    data = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjYGBg+A8AAQQBAHAgZQsAAAAASUVORK5CYII="  # png
+    im = Image.open(BytesIO(base64.b64decode(data)))
+
+    assert im.mode == "RGBA"
