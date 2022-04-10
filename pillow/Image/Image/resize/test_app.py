266dc14c8cff135c37f11ef27b16e1a32387b5aa
jongwoo Yoo <dbwhddn10@gmail.com>
2022-04-10 19:49:26 +0900

    feat(pillow): add Image/Image/resize example

---
+++ pillow/Image/Image/resize/test_app.py
@@ -0,0 +1,20 @@
+import base64
+from io import BytesIO
+
+from PIL import Image
+
+
+def test_case():
+    data = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjYGBg+A8AAQQBAHAgZQsAAAAASUVORK5CYII="  # png
+    im = Image.open(BytesIO(base64.b64decode(data)))
+    im = im.resize((100, 100))
+
+    assert (100, 100) == im.size
+
+    im = im.resize((200, 100))
+
+    assert (200, 100) == im.size
+
+    im = im.resize((100, 200))
+
+    assert (100, 200) == im.size
